b594ac24449257d659ed2e6ea315a8be38877b01
FaYMan2 <suavarnavats244@gmail.com>
2024-07-11 09:08:12 +0530

    deployment files

--- main.py
+++ main.py
@@ -1,14 +1,58 @@
-from typing import Optional
-
-from fastapi import FastAPI
-
+from fastapi import FastAPI,HTTPException
+import json
+from vectore_store import get_Vectors
+from embedder import Embedder
+from dotenv import load_dotenv
+import os
+from fastapi.middleware.cors import CORSMiddleware
+load_dotenv()
+process_key = os.getenv('API_KEY')
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def ping():
+    return{
+        "message" : "server active"
+    }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/vectors/{docId}")
+async def root(docId : str,api_key : str):
+    if process_key == api_key:
+        vectors = await get_Vectors(namespace=docId)
+        print(vectors)
+        return {"message": json.dumps(vectors)}
+    else:
+        raise HTTPException(status_code=401,detail="Unortharized access - invalid api key")
+
+
+@app.post("/emmbed")
+async def embedd(url : str,docId : str,api_key : str):
+    print('ROUTE CALLED')
+    if process_key == api_key:
+        res = await Embedder(URL=url,
+                             chunkSize=10000,
+                             overlap=500,
+                             namespace=docId)
+        if res:
+            return {
+                "vector_count" : res['vector_count'],
+                "time" : res['time'],
+                "namespace" : res['namespace']
+             }
+        
+        raise HTTPException(status_code=500,detail="OOPS : SOMETHING WENT WRONG")
+        
+    else :
+        raise HTTPException(status_code=401,detail="Unortharized access - invalid api key")
+        
